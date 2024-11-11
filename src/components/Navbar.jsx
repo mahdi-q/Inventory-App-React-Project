2a1d75c680ac275ac9d754e057983b1378ec2e5d
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-11-11 20:19:42 +0330

    Develope "Navbar" component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div className="h-12 flex items-center justify-center bg-slate-900 mb-6">
+      <h1 className="text-slate-300 font-bold text-2xl">
+        Inventory App Projecct
+      </h1>
+    </div>
+  );
+}
+export default Navbar;
